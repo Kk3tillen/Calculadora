6bdb887a08c2cc37f8b558558ef8fd025bf43c4a
Kézia Ketillen <k.kezia@academico.ifrn.edu.br>
2022-06-04 23:54:52 -0300

    initial commit

---
+++ src/main/java/TesteC.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author kezia
+ */
+public class TesteC {
+
+    public static void main(String[] args) {
+
+        double num2 = 10;
+        double div = 0;
+
+        double c = Calculadora.div(num2, div);
+
+    }
+}
